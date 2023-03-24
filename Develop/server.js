fed443ca18ad8c7de2bda4ab3470276301619bba
blayne-04 <befuller2004@gmail.com>
2023-03-23 20:39:05 -0700

    made server progress, get requests working

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,36 @@
+//dependencies
 const express = require('express')
-const path = require("path")
-const landingPage = require('./public/index.html')
+const path = require('path')
+const fs = require('fs')
+//port
 const PORT = 3001
-
+//express()
 const app = express()
-app.use(express.urlencoded({ extended: true }));
+//middleware
+app.use(express.static('public'));
 app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', async(req, res) => {
+    const dbPath = path.join(__dirname, 'db/db.json');
+    fs.readFile(dbPath, 'utf8', (data, err) => {
+        const notes = JSON.parse(data);
+    })
+    res.json(notes);
+})
+
+app.post('/notes', async (req, res) => {
+    try{
+        res.status(200).json(req.body)
+    } catch{
+        res.status(400).json(err)
+    }
+})
+
+
+app.listen(PORT, () =>
+console.log(`Serving static asset routes at http://localhost:${PORT}!`)
+)
