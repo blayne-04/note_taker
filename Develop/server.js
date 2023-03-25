4d7b2e34654d08a9efc5abe7ead837e263daf570
blayne-04 <befuller2004@gmail.com>
2023-03-24 20:44:32 -0700

    added backend support for saving data and displaying it on left side

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 //dependencies
 const express = require('express')
 const path = require('path')
+const dbPath = path.join(__dirname, 'db/db.json');
 const fs = require('fs')
 //port
 const PORT = 3001
@@ -14,22 +15,34 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', async(req, res) => {
-    const dbPath = path.join(__dirname, 'db/db.json');
-    fs.readFile(dbPath, 'utf8', (data, err) => {
+app.get('/api/notes', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw err;
         const notes = JSON.parse(data);
+        res.status(200).json(notes)
     })
-    res.json(notes);
 })
 
-app.post('/notes', async (req, res) => {
-    try{
-        res.status(200).json(req.body)
-    } catch{
-        res.status(400).json(err)
-    }
-})
-
+app.post('/api/notes', async (req, res) => {
+        fs.readFile(dbPath, 'utf8', (err, data) => {
+            if(err){
+                console.error(err);
+                res.status(500).send("Invalid Data")
+            }else{
+                const notes = JSON.parse(data)
+                notes.push(req.body)
+                fs.writeFile(dbPath, JSON.stringify(notes, null, 4), (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send('Error writing to file');
+                    } else {
+                        console.log('Data updated');
+                        res.status(200).send('\n Data updated');
+                    }
+                });
+            }
+        });
+});
 
 app.listen(PORT, () =>
 console.log(`Serving static asset routes at http://localhost:${PORT}!`)
